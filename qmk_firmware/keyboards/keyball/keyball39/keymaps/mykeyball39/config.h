25ed3c71ad17e3bcc47ee200690191e304fed902
funeiri <funeiri2016@yahoo.co.jp>
2025-10-05 12:39:44 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/mykeyball39/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/mykeyball39/config.h
@@ -38,7 +38,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define OLED_TIMEOUT 120000
 #define TAPPING_TERM 180
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 4
+#define AUTO_MOUSE_DEFAULT_LAYER 2
 #define COMBO_ONLY_FROM_LAYER 0
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
 #define KEYBALL_SCROLL_DIV_DEFAULT 5
